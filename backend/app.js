0f36b0234cda138f62940c7a52729a9e540f97b2
Jabez Sanjay <jbzsanjay2000@gmail.com>
2022-01-08 23:17:41 +0530

    Basic backend setup

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+//setup express
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+//Swagger setup
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//Regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookies and File middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+//Morgan middleware
+app.use(morgan('tiny'));
+
+module.exports = app;
