cb4271d1f36fce0bc1c72aac6e82d7f0d650b92f
Jabez Sanjay <jbzsanjay2000@gmail.com>
2022-01-09 01:17:57 +0530

    Completed Hooks and functions in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,5 +50,39 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+//encrypt password using bcrypt
+//Hooks are functions that run before or after a certain action
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//Validate the password
+userSchema.methods.validatePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//Create and return JWT token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
+
+//Generate forgot password token
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+  //Getting a hash = make sure to get a hash on backend
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  //time of token expiry
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
 
 module.exports = mongoose.model('User', userSchema);
