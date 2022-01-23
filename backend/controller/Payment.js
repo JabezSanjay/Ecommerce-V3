3a94d7b782976d35df92b988493e7407ea70fc36
Jabez Sanjay <jbzsanjay2000@gmail.com>
2022-01-23 22:00:05 +0530

    Completed razorpay payment

--- backend/controller/Payment.js
+++ backend/controller/Payment.js
@@ -1,5 +1,7 @@
 const BigPromise = require('../middleware/bigPromise');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Razorpay = require('razorpay');
+const { nanoid } = require('nanoid');
 
 exports.sendStripeKey = BigPromise(async (req, res, next) => {
   res.status(200).json({
@@ -19,5 +21,30 @@ exports.captureStripePayment = BigPromise(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: charge,
+  });
+});
+
+exports.sendRazorpayKey = BigPromise(async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: process.env.RAZORPAY_KEY_ID,
+  });
+});
+
+exports.captureRazorpayPayment = BigPromise(async (req, res, next) => {
+  let instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+  const { amount } = req.body;
+  const options = {
+    amount,
+    currency: 'INR',
+    receipt: nanoid(),
+  };
+  const payment = await instance.orders.create(options);
+  res.status(200).json({
+    status: 'success',
+    data: payment,
   });
 });
--- backend/routes/Payment.js
+++ backend/routes/Payment.js
@@ -2,12 +2,18 @@ const express = require('express');
 const {
   sendStripeKey,
   captureStripePayment,
+  sendRazorpayKey,
+  captureRazorpayPayment,
 } = require('../controller/Payment');
 const { isLoggedIn, customRole } = require('../middleware/user');
 const router = express.Router();
 
 //Stripe routes
-router.route('/get/stripe/key').get(sendStripeKey);
+router.route('/get/stripe/key').get(isLoggedIn, sendStripeKey);
 router.route('/create/stripe').post(isLoggedIn, captureStripePayment);
+
+//Razorpay routes
+router.route('/get/razorpay/key').get(isLoggedIn, sendRazorpayKey);
+router.route('/create/razorpay').post(isLoggedIn, captureRazorpayPayment);
 
 module.exports = router;
